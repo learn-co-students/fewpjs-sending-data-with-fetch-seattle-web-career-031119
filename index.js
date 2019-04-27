7c4df4a750306330fe5411a14dd37b4b656d04ae
github username <github email address>
2019-04-27 11:31:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+let configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    userName: userName,
+    userEmail: userEmail
+  })
+};
+
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(ret => ret.json())
+  .then( json => {
+    let ul = document.getElementById('user-ids')
+    let newLi = document.createElement('li')
+    newLi.textContent = json.id
+    ul.appendChild(newLi)
+  })
+  .catch(error => {
+    let errorDiv = document.getElementById('error-message')
+    errorDiv.textContent = error.message
+  })
+}
