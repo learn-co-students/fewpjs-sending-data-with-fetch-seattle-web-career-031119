ef9251ff09ea2082f3ca2262daee768c32833a2a
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-04-28 16:25:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+// examples of how to save parts of the post request to variables
+
+// const formData = {
+//   name: "mera",
+//   email: "sample@gmail.com"
+// }
+//
+// const configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// }
+// fetch("url string", configObj)
+
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+    .then(res => res.json())
+    .then(json => handleResponse(json))
+    .catch((er) => displayError(er))
+}
+
+function handleResponse(json) {
+  const p = document.createElement('p')
+  p.textContent = json.id
+
+  const body = document.getElementById('body')
+  body.appendChild(p)
+
+}
+
+function displayError(er) {
+  const p = document.createElement('p')
+  p.textContent = er.message
+
+  const body = document.getElementById('body')
+  body.appendChild(p)
+}
+
+// testing
+// submitData("mera", "sample")
